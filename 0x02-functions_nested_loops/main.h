3c16dc719b747c0cff3bf1892dfbc8cf4dea93b4
MaryKwawu <may419316@gmail.com>
2022-12-13 20:50:47 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+int main(void);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+void _putchar(int n);
+
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
