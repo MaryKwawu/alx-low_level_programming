565b26986f8c2cdf2a86d10cf8a8b0311d18d39e
MaryKwawu <may419316@gmail.com>
2022-12-21 15:58:03 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,14 +1,17 @@
-int _putchar(char);
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char *_strncpy(char *dest, char *src,int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *str);
-char *cap_string(char *str);
-char *leet(char *s);
-char *rot13(char *s);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
 void print_number(int n);
-void rev_string(char *n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_line(char *c, int s, int l);
-void print_butter(char *b, int size);
+
+#endif
