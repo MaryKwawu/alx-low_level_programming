3d90c66c12ac93ca8e6cc25bae6e5d5ca9903020
MaryKwawu <may419316@gmail.com>
2023-02-02 09:33:57 -0800

    101-password

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,8 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
